838a374c75e004e3a24e495617af249da28dbcc9
CC <chen@zervice.us>
2024-07-01 09:14:46 +0800

    1、add templates

--- components/sections/Template.js
+++ components/sections/Template.js
@@ -1,7 +1,7 @@
 import "twin.macro";
 import Container from "~/components/Atoms/Container";
 import tw, {css} from "twin.macro";
-import useTranslate from "~/hooks/useTranslate";
+import React from "react";
 import Image from "next/image";
 
 const style = {
@@ -31,7 +31,7 @@ const style = {
             ${tw`shadow-xl rounded-lg shadow-gray-400`}
           }
           ul {
-            ${tw`grid grid-cols-4 gap-4 justify-center`}
+            ${tw`grid grid-cols-4 gap-4 justify-center `}
           }
         `,
     ],
@@ -57,25 +57,20 @@ export default function Template({locale}) {
     ]
     const data2 = [
         {
-            title: "Website Assistant",
-            introduce: "Quickly build a website assistant using a Web (URL) and FAQ",
+            title: "WHO Health Topic",
+            introduce: "Quickly build a WHO health topic assistant using a Web (URL)",
             url: "https://app.promptai.us/templates/p_dn1qvapxdxj4",
-            image: "/images/web.svg"
+            image: "/images/line-health.svg"
+        }, {
+            title: "Ollama",
+            introduce: "Using inquiry as a replacement for referencing documents: a more efficient learning way.",
+            url: "https://app.promptai.us/templates/p_dq4q6b0ue22o",
+            image: "/images/ollama.svg"
         }, {
             title: "Survey",
-            introduce: "The fastest way to create a conversational bot is providing a GPT prompt.",
-            url: "https://app.promptai.us/templates/p_dn1t6ybs9iww",
-            image: "/images/survey.svg"
-        }, {
-            title: "Survey",
-            introduce: "The fastest way to create a conversational bot is providing a GPT prompt.",
-            url: "https://app.promptai.us/templates/p_dn1t6ybs9iww",
-            image: "/images/survey.svg"
-        }, {
-            title: "Survey",
-            introduce: "The fastest way to create a conversational bot is providing a GPT prompt.",
-            url: "https://app.promptai.us/templates/p_dn1t6ybs9iww",
-            image: "/images/survey.svg"
+            introduce: " a streamlined tool designed to efficiently answer all your questions on housing laws and regulations.",
+            url: "https://app.promptai.us/templates/p_dqcmq1jpj400",
+            image: "/images/house.svg"
         }
     ]
     return <section>
@@ -94,8 +89,9 @@ export default function Template({locale}) {
                                    window.open(item.url, "_blank")
                                }}>
                         <div>
-                            <Image src={item.image} alt={""}/>
+                            <Image src={item.image} width="32" height="32" alt={""}/>
                             <Image id={index + "-ul-1"}
+                                   width="32" height="32"
                                    style={{display: "none"}}
                                    src={"/images/openRightMini.svg"}
                                    alt={""}/>
@@ -108,33 +104,34 @@ export default function Template({locale}) {
                 })}
             </ul>
 
-            {/*<ul css={[style.templates]} className="grid grid-cols-4 gap-4 justify-center ">*/}
-            {/*    {data2.map((item, index) => {*/}
-            {/*        return <li key={index}*/}
-            {/*                   onMouseOver={() => {*/}
-            {/*                       document.getElementById(index + "-ul-2").style.display = 'block';*/}
-            {/*                   }}*/}
-            {/*                   onMouseOut={() => {*/}
-            {/*                       document.getElementById(index + "-ul-2").style.display = 'none';*/}
-            {/*                   }}*/}
-            {/*                   className={"rounded-lg"}*/}
-            {/*                   onClick={() => {*/}
-            {/*                       window.open(item.url, "_blank")*/}
-            {/*                   }}>*/}
-            {/*            <div>*/}
-            {/*                <Image src={item.image} alt={""}/>*/}
-            {/*                <Image id={index + "-ul-2"}*/}
-            {/*                       style={{display: "none"}}*/}
-            {/*                       src={"/images/openRightMini.svg"}*/}
-            {/*                       alt={""}/>*/}
-            {/*            </div>*/}
-            {/*            <h3>*/}
-            {/*                <span>{item.title}</span>*/}
-            {/*            </h3>*/}
-            {/*            <p tw={"text-ellipsis"}>{item.introduce}</p>*/}
-            {/*        </li>;*/}
-            {/*    })}*/}
-            {/*</ul>*/}
+            <ul css={[style.templates]} className="grid grid-cols-4 gap-4 justify-center ">
+                {data2.map((item, index) => {
+                    return <li key={index}
+                               onMouseOver={() => {
+                                   document.getElementById(index + "-ul-2").style.display = 'block';
+                               }}
+                               onMouseOut={() => {
+                                   document.getElementById(index + "-ul-2").style.display = 'none';
+                               }}
+                               className={"rounded-lg"}
+                               onClick={() => {
+                                   window.open(item.url, "_blank")
+                               }}>
+                        <div>
+                            <Image src={item.image} width="32" height="32" alt={""}/>
+                            <Image id={index + "-ul-2"}
+                                   width="32" height="32"
+                                   style={{display: "none"}}
+                                   src={"/images/openRightMini.svg"}
+                                   alt={""}/>
+                        </div>
+                        <h3>
+                            <span>{item.title}</span>
+                        </h3>
+                        <p tw={"text-ellipsis"}>{item.introduce}</p>
+                    </li>;
+                })}
+            </ul>
         </Container>
     </section>;
 
